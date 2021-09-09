3884934774729f048dede709cf2179ccce43826d
Evangelos Dimitriou <evangelos.dimitriou@zuehlke.com>
2021-09-09 15:58:02 +0200

    Added echo comments to debug install command

--- cdk_project/pipeline_stack.py
+++ cdk_project/pipeline_stack.py
@@ -25,6 +25,6 @@ class PipelineStack(core.Stack):
             synth_action=pipelines.SimpleSynthAction(
                 source_artifact=source_artifact,
                 cloud_assembly_artifact=cloud_assembly_artifact,
-                install_command='npm install -g aws-cdk && pip install -r requirements.txt',
+                install_command='npm install -g aws-cdk && echo done && echo starting pip && pip install -r requirements.txt',
                 synth_command='cdk synth'))    
             
