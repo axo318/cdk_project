fd4b0fc0ea6ddb94c81e8b771da0fa95d355aa9a
Evangelos Dimitriou <evangelos.dimitriou@zuehlke.com>
2021-09-09 15:39:12 +0200

    Initial Commit

--- cdk_project/cdk_project_stack.py
+++ cdk_project/cdk_project_stack.py
@@ -1,15 +1,26 @@
-from aws_cdk import core as cdk
+from os import path
 
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
 from aws_cdk import core
+import aws_cdk.aws_lambda as lmb
+import aws_cdk.aws_apigateway as apigw
 
 
-class CdkProjectStack(cdk.Stack):
+class CdkProjectStack(core.Stack):
 
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        this_dir = path.dirname(__file__)
+
+        handler = lmb.Function(self, 'Handler',
+            runtime=lmb.Runtime.PYTHON_3_7,
+            handler='handler.handler',
+            code=lmb.Code.from_asset(path.join(this_dir, 'lambda')))
+
+        gw = apigw.LambdaRestApi(self, 'Gateway',
+            description='Endpoint for a simple Lambda-powered web service',
+            handler=handler.current_version)
+
+        self.url_output = core.CfnOutput(self, 'Url',
+            value=gw.url)
